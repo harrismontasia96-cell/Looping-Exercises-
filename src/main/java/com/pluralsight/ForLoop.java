e30644beb2cd126da79b8690a4fd26ee78e75021
montasia harris <mharris@my.yearupunited.com>
2025-10-02 10:59:10 -0400

    WRKBKWK2 While Loop Exercise

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -1,13 +1,15 @@
 package com.pluralsight;
 
 public class ForLoop {
-    public static void main(String[] args) {
-        for (int i = 10; i >= 1; i--) {
+    public static void main(String[] args)throws InterruptedException {
+        for (int i = 10; i >= 1; i--)  {
+            Thread.sleep(1000);
             System.out.println(i);
         }
         System.out.println("launch");
 
         for (int i = 20; i >= 8; i--) {
+            Thread.sleep(1000);
             System.out.println(i);
         }
         System.out.println("launch");
