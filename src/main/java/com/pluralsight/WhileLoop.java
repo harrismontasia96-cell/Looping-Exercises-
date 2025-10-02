93b788958b2fbb4963de8a8527521e4c4b791d0b
montasia harris <mharris@my.yearupunited.com>
2025-10-02 10:19:47 -0400

    WRKBKWK2 While Loop Exercise

--- src/main/java/com/pluralsight/WhileLoop.java
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
 public class WhileLoop {
+    public static void main(String[] args) {
+        int mascara;
+        mascara = 3;
+        while (mascara < 5)
+        System.out.println("I love Java");
+        int i;
+        i = mascara +=1;
+
+
+
+
+
+
+    }
 }
